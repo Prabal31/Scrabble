cc196f0175088629e5341fb5ff8d391208ab309a
Prabal <manchandaprabal878@gmail.com>
2024-06-29 23:15:40 -0400

    Refactor scrabble.py to include dictionary mapping letters to points and calculate total points for each player

--- scrabble.py
+++ scrabble.py
@@ -4,14 +4,12 @@ points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 
 letter_to_points={key:values for key, values in zip(letters,points)}
 
 letter_to_points[""]=0
-print(letter_to_points)
 
 def score_word(word):
   point_total=0
   for letters in word:
     point_total+=letter_to_points.get(letters.upper(), 0)
   return point_total
-
 brownie_points=score_word("BROWNIE")
 print(brownie_points)
 
@@ -21,3 +19,15 @@ player_to_words = {
     'Lexi Con': ['ERASER', 'BELLY', 'HUSKY'],
     'Prof Reader': ['ZAP', 'COMA', 'PERIOD']
 }
+player_to_points={}
+
+for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player]=player_points
+print(player_to_points)
+
+
+
+
